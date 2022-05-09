bd8d1b6ecc59c8e51eee2edb6a7bd583b1e44429
Attmoon <gus89580@naver.com>
2022-05-09 23:42:51 +0900

    Update Ex03Cotroller.java

--- src/main/java/org/zerock/controller/ex01/Ex03Cotroller.java
+++ src/main/java/org/zerock/controller/ex01/Ex03Cotroller.java
@@ -17,7 +17,7 @@ public class Ex03Cotroller {
 		System.out.println("/ex03/sub01 경로, name 파라미터 있음");
 	}
 	
-	@RequestMapping(value = "sub01", params = {"name", "age"}) // 쿼리스트링으로 name이나 age파라미터가 있으면 실행됨
+	@RequestMapping(value = "sub01", params = {"name", "age"}) // 쿼리스트링으로 name과 age파라미터가 둘다 있으면 실행됨
 	public void method03() {
 		System.out.println("같은 경로, name, age 파라미터 있음");
 	}
