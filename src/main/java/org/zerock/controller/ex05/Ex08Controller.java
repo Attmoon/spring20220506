a7cd56206db5698d2c3afff8ce2f9c5937139dfa
Attmoon <99157555+Attmoon@users.noreply.github.com>
2022-05-10 11:51:48 +0900

    add attribute

--- src/main/java/org/zerock/controller/ex05/Ex08Controller.java
+++ src/main/java/org/zerock/controller/ex05/Ex08Controller.java
@@ -1,10 +1,17 @@
 package org.zerock.controller.ex05;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.zerock.controller.ex03.Customer;
+import org.zerock.controller.ex03.Employee;
 
 @Controller
 @RequestMapping("ex08")
@@ -37,6 +44,7 @@ public class Ex08Controller {
 		return "ex08/sub03";
 	}
 	
+	// controller가 일한결과를 model에 담아서 view에 전달
 	// 요청경로 : /ex08/sub04
 	// view name : /WEB-INF/views/ex08/sub04.jsp
 	@RequestMapping("sub04")
@@ -50,4 +58,92 @@ public class Ex08Controller {
 	public void method05(Model model) {
 		model.addAttribute("heroList", new String[] {"Ironman", "Thor", "Captain"});
 	}
+	
+	@RequestMapping("sub06")
+	public void method06(Model model) {
+		
+		Map<String, String> map = new HashMap<>();
+		map.put("name", "jin");
+		map.put("song", "butter");
+		map.put("group", "bts");
+		
+		model.addAttribute("desc", map);
+		
+	}
+	
+	@RequestMapping("sub07")
+	public void method07(Model model) {
+		
+		Customer cus = new Customer();
+		cus.setAddress("seoul");
+		cus.setAge(30);
+		cus.setName("donald");
+		
+		model.addAttribute("customer", cus);
+		
+	}
+	
+	// model에 값을넣어서 controller가 view에게 넘겨준다.
+	@RequestMapping("sub08")
+	public void method08(Model model) {
+		Employee emp = new Employee();
+//		email, name, salary
+		
+		emp.setEmail("gus1@naver.com");
+		emp.setName("suhyun");
+		emp.setSalary(30000);
+		
+		model.addAttribute("employee", emp);
+		// jsp와 이메소드를 완성해서 위에 세팅한 email, name, salary 출력되도록
+		
+	}
+	
+	@RequestMapping("sub09")
+	public String method09(Model model) {
+		Employee emp = new Employee();
+		emp.setEmail("korea@daum.net");
+		emp.setName("sunja");
+		emp.setSalary(70000);
+		
+		model.addAttribute(emp); // attribute이름이 클래스의 이름으로 결정(단, lowerCamelCase형식)
+//		model.addAttribute("employee", emp); 와 같음
+		
+		return "ex08/sub08";
+	}
+	
+	@RequestMapping("sub10")
+	public String method10(Model model) {
+		
+		// model.addAttribute() 사용해서
+		// sub07.jsp에서 고객의 정보가 출력되도록 코드를 완성
+		
+		Customer cus = new Customer();
+		cus.setAddress("busan");
+		cus.setAge(40);
+		cus.setName("whisper");
+		
+		model.addAttribute(cus);
+		return "ex08/sub07";
+	}
+	
+	@RequestMapping("sub11")
+	public void method11(Model model) {
+		List<String> l = new ArrayList<>();
+		
+		l.add("wanda");
+		l.add("doctor");
+		l.add("wong");
+		
+		model.addAttribute("list", l);
+		
+	}
+	
+	@RequestMapping("sub12")
+	public void method12() {
+		// 1. request 파라미터 수집/가공
+		// 2. bussiness 로직처리
+		// 3. add attribute
+		// 4. forward / redirect
+	}
+	
 }
---
+++ src/main/java/org/zerock/controller/ex05/Ex09Cotroller.java
@@ -0,0 +1,71 @@
+package org.zerock.controller.ex05;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.zerock.controller.ex03.Customer;
+import org.zerock.controller.ex03.Employee;
+
+@Controller
+@RequestMapping("ex09")
+public class Ex09Cotroller {
+	
+	@RequestMapping("sub01")
+	public void method01(String name, Model model) {
+		// 1. request 파라미터 수집/가공
+		System.out.println(name);
+		
+		// 3. model에 attribute 추가
+		model.addAttribute("name", name);
+		
+		// 4. view forward : ex09/sub01.jsp
+	}
+	
+	// /ex09/sub02?name=sunja
+	@RequestMapping("sub02")
+	public String method02(@ModelAttribute("name") String name) { 
+		// model에 attribute를 더해주는일을 어노테이션이 대신해줌
+		// model.addAttribute("name", name);
+		
+		return "ex09/sub01";
+	}
+	
+	// /ex09/sub03?address=seoul&email=gmail.com
+	@RequestMapping("sub03")
+	public void method03(@ModelAttribute("address") String address, 
+			             @ModelAttribute("email") String email) {
+		
+	}
+	
+	// /ex09/sub04?name=trump&age=40&address=newyork
+	@RequestMapping("sub04")
+	public void method04(@ModelAttribute("customer") Customer cus) {
+		// ModelAttribute에 customer라는 이름으로 Customer빈의 모든걸 넣겠다
+		// 리턴타입 void로 쓰면 경로가 view이름으로 됨
+	}
+	
+	// /ex09/sub05?name=trump&age=40&address=newyork
+	@RequestMapping("sub05")
+	public String method05(@ModelAttribute Customer cus) { // @modelattribute의 이름 생략
+		return "ex09/sub04"; // 리턴타입 string으로 써주면 view경로를 설정해주기위함이고
+	}
+	
+	@RequestMapping("sub06")
+	public String method06(Customer cus) { // @modelattribute 생략
+		return "ex09/sub04";
+	}
+	
+	// 요청경로 : /ex09/sub07
+	// 쿼리스트링 : ?name=sunja&salary=50000&email=sunja@gmail.com
+	
+	// method07 작성
+	// Employee 객체가
+	// 쿼리스트링으로 넘어온 파라미터값들을 세팅해서
+	// model에 "employee"라는 이름의 attribute로 추가되고
+	// /ex09/sub07 뷰로 포워드 되도록
+	@RequestMapping("sub07")
+	public void method07(Employee emp) {
+		
+	}
+}
